15e6cdc15223c7e8d8f4237110a5eb0b9f23a50a
Irene Mayor <ihmayor@ucalgary.ca>
2016-03-30 15:48:56 -0600

    Sample of Data format given

--- test.bat
+++
@@ -1,5 +0,0 @@
-setlocal enabledelayedexpansion
-set output=
-for /f "delims=" %%a in ('git diff --name-only HEAD) do ( set output=!output! "%%a" )
-git archive -o update.zip HEAD %output%
-endlocal
